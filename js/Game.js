c83b60278330361a92165641b6b858b31763eb4f
kritikasharma1234 <75725096+kritikasharma1234@users.noreply.github.com>
2021-03-31 15:57:16 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,38 @@
+class Game {
+    constructor(){}
+    
+    getState(){
+      var gameStateRef  = database.ref('gameState');
+      gameStateRef.on("value",function(data){
+         gameState = data.val();
+      })
+     
+    }
+  
+    update(state){
+      database.ref('/').update({
+        gameState: state
+      });
+    }
+  
+    start(){
+      if(gameState === 0){
+        player = new Player();
+        player.getCount();
+        form = new Form()
+        form.display();
+      }
+    }
+    play(){
+      form.hide();
+      textsize(30);
+      text("GameStart",120,100);
+      player.getplayerInfo();
+
+      if(allPlayers !== undefined){
+        var display_position = 130;
+        display_position+=20;
+        textsize(15);
+            text(allPlayers[plr].name+""+ allPlayers[plr].distance,120,display_position)
+    }
+  } 
